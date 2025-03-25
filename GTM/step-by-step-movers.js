9991269d7f0bc199e1833d8f5006bb89ddf7173c
Carloss404 <aar@whitesharkmedia.com>
2025-03-25 16:09:16 -0600

    add GTM custom code

---
+++ GTM/step-by-step-movers.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function () {
+	var gsc = document.createElement("meta");
+	gsc.name = "google-site-verification";
+	gsc.content = "amXyabxFQyC2lAlbETGIVclmQO2AzoBCIlVaB_RVlm0";
+	document.getElementsByTagName("head")[0].appendChild(gsc);
+});
