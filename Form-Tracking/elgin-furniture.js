775193a6cdcbe52fed0bed344b12fd720b180673
Carloss404 <aar@whitesharkmedia.com>
2025-05-01 16:33:43 -0600

    new tracking form script

---
+++ Form-Tracking/elgin-furniture.js
@@ -0,0 +1,31 @@
+var form = document.getElementById("contactUsForm");
+
+form.addEventListener("submit", function () {
+	var firstName = document.getElementById("FirstName").value;
+	var lastName = document.getElementById("LastName").value;
+	var fullName = firstName + " " + lastName;
+	var email = document.getElementById("EmailAddress").value;
+	var phone = document.getElementById("PhoneNumber").value;
+	var message = document.getElementById("Message").value;
+
+	if (fullName && email && phone) {
+		try {
+			__ctm.form.track(
+				"app.calltrackingmetrics.com", // the capture host
+				"FRT472ABB2C5B9B141A903540746653AC7A76F6FC0DE47F848627FAE996E2373680", // this FormReactor
+				"2162424790",
+				{
+					country_code: "1", // the expected country code e.g. +1, +44, +55, +61, etc... the plus is excluded
+					name: fullName,
+					phone: phone,
+					email: email,
+					custom: {
+						message: message,
+					},
+				}
+			);
+		} catch (error) {
+			console.error("Error tracking form submission:", error);
+		}
+	}
+});
