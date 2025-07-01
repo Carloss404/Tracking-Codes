899a94100bcebdbd3fe6263000bb37a6212712c1
Charlesss-0 <aragonaugusto588@gmail.com>
2025-06-30 21:12:25 -0600

    add new form tracking code

---
+++ Form-Tracking/gavigan-furniture.js
@@ -0,0 +1,35 @@
+(window.__ctm_loaded || []).push(function () {
+	document.getElementById("contactUsFormSubmit").addEventListener("click", function () {
+		var firstName = document.getElementById("FirstName").value;
+		var lastName = document.getElementById("LastName").value;
+		var name = firstName + " " + lastName;
+
+		var email = document.getElementById("EmailAddress").value;
+		var phone = "8888888888";
+		var message = document.getElementById("Message").value;
+
+		if (!(name && phone && email)) {
+			console.error("Missing required fields");
+			return;
+		}
+
+		try {
+			__ctm.form.track(
+				"app.calltrackingmetrics.com", // the capture host
+				"FRT472ABB2C5B9B141A48DF2D172722629830858D0F6ED7C8C21841D33C9A6338E5", // this FormReactor
+				"4106304132",
+				{
+					country_code: "1", // the expected country code e.g. +1, +44, +55, +61, etc... the plus is excluded
+					name: name,
+					phone: phone,
+					email: email,
+					custom: {
+						message: message,
+					},
+				}
+			);
+		} catch (error) {
+			console.error("Error tracking form submission:", error);
+		}
+	});
+});
