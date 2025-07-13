6f58ecb06afb35b586fd880517b0fe4567004a61
Charlesss-0 <aragonaugusto588@gmail.com>
2025-07-12 22:56:43 -0600

    update tracking codes

--- gtm-tracking-codes/convert-to-clickable-phone-number.js
+++ gtm-tracking-codes/convert-to-clickable-phone-number.js
@@ -8,7 +8,7 @@
 	var regexPatterns = [/(Call us at\s*)([\d\-().\s]{10,})/g, /(Call\s*)([\d\-().\s]{10,})/g];
 
 	regexPatterns.forEach(function (pattern) {
-		body.innerHTML = body.innerHTML.replace(pattern, function (fullMatch, prefix, phoneNumber) {
+		body.innerHTML = body.innerHTML.replace(pattern, function (_, prefix, phoneNumber) {
 			var telLink = phoneNumber.replace(/[^\d]/g, "");
 
 			var linkElement = '<a href="tel:' + telLink + '">' + phoneNumber + "</a>";
