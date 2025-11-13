d0c6515c3971ba733ef438cd9087430609653635
Carloss404 <aar@whitesharkmedia.com>
2025-11-13 13:04:01 -0600

    add pronto insurance form tracking script

--- gtm-tracking-codes/convert-to-clickable-phone-number.js
+++ gtm-tracking-codes/convert-to-clickable-phone-number.js
@@ -5,7 +5,7 @@
 		return;
 	}
 
-	var regexPatterns = [/(Call us at\s*)([\d\-().\s]{10,})/g, /([\d\-().\s]{10,})/g];
+	var regexPatterns = [/(US Cell \s*)([\d\-().\s]{10,})/g, /([\d\-().\s]{10,})/g];
 
 	regexPatterns.forEach(function (pattern) {
 		body.innerHTML = body.innerHTML.replace(pattern, function (_, prefix, phoneNumber) {
